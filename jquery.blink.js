e45e21351ffc8c7e9532ff9618d538d59df0dfd1
tianyingchun <lovely6rui@outlook.com>
2013-01-04 14:00:55 +0800

    Jquery plugin  blink timer
    Jquery plugin  blink timer

---
+++ jquery.blink.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    var blinkTimers = new Object();
+
+    $.fn.blink = function (options) {
+        var defaults = { delay: 500, cssClass: 'ui-state-hover' };
+        var options = $.extend(defaults, options);  
+        return this.each(function () {
+            var obj = $(this);
+            if (blinkTimers[obj] == undefined) {
+                blinkTimers[obj] = setTimeout(function () {
+                    obj.toggleClass(options.cssClass); 
+                    blinkTimers[obj] = setTimeout(arguments.callee, options.delay); 
+                }, options.delay);
+            }
+        });
+    };
+    $.fn.stopBlink = function (options) {
+        return this.each(function () {
+            var obj = $(this);
+            clearTimeout(blinkTimers[obj]);
+            delete blinkTimers[obj];
+        });
+    };
+})(jQuery);
