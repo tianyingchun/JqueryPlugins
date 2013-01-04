bc660973d67b6f5c7a4f129fd39fe0cc23141763
tianyingchun <lovely6rui@outlook.com>
2013-01-04 18:27:02 +0800

    format code
    format code

--- Z.js
+++ Z.js
@@ -1,11 +1,11 @@
-(function(){
- 	/**
+(function () {
+    /**
  	 *  Declare Global Object "Z" that provider some basic methods used to organize js modules files.
      *  Imortant Methods: 'Z.extend(subClass,supperClass)', 'Z.define(ns,function)'
      *  Finnaly, we need compile all modules into live/all.js with specific orders.
      *  libs/lib.js contains jQuery.js,underscore.js,backbone.js
  	 */
- 	var global = this,
+    var global = this,
         enumerables = ['hasOwnProperty', 'valueOf', 'isPrototypeOf', 'propertyIsEnumerable',
                        'toLocaleString', 'toString', 'constructor'];
     if (typeof Z === 'undefined') {
@@ -38,7 +38,7 @@
      * Extends Z utities methods. 
      */
     Z.apply(Z, {
-     	applyIf: function (object, config) {
+        applyIf: function (object, config) {
             var property;
             if (object) {
                 for (property in config) {
@@ -54,7 +54,7 @@
          * @param  {string} str the namespace string. eg. "Z.Util.Manager"
          * @return {Object}  return namespace object.eg. Z.Util.Manager
          */
-        ns: function (str) { 
+        ns: function (str) {
             str = str.replace(/^\s+|\s+$/ig, "");
             if (str) {
                 var arr = str.split("."), o = Z;
@@ -71,7 +71,7 @@
          * @param  http url eg.http://my.com/home.aspx?name=terence&categoryid=10
          * @return {Object} return serialize result. eg.{'name':'terence','categoryid':'10'}
          */
-		serialize: function (url) {
+        serialize: function (url) {
             var params = {};
             var regex = new RegExp("\\b(\\w+)=([^&#]+)", "ig");
             var match;
@@ -102,24 +102,24 @@
 		 * @param  {function} supperClass
 		 * @return {function} subClass
 		 */
-		extend:function (subClass,supperClass){
-			var F=function(){};
-			F.prototype=supperClass.prototype;
-			subClass.prototype=new F();
-			subClass.prototype.constructor=subClass; 
-			subClass.supperClass=supperClass.prototype; 
-			if(supperClass.prototype.constructor==Object.prototype.constructor){
-				supperClass.prototype.constructor=supperClass;
-			}
-			return subClass;
-		},
-		/**
+        extend: function (subClass, supperClass) {
+            var F = function () { };
+            F.prototype = supperClass.prototype;
+            subClass.prototype = new F();
+            subClass.prototype.constructor = subClass;
+            subClass.supperClass = supperClass.prototype;
+            if (supperClass.prototype.constructor == Object.prototype.constructor) {
+                supperClass.prototype.constructor = supperClass;
+            }
+            return subClass;
+        },
+        /**
 		 * Define fucntion used to define contructor class within specific namespace.
 		 * @param  {string} ns  namespace string. eg. Z.Model.Login
 		 * @param  {function} function, need to return function constructor 
 		 * @return {function} the function attached to Z.Model.Login
 		 */
-		define: function (ns, func) {
+        define: function (ns, func) {
             if (!func) throw new Error("Miss func parameter for Z.define");
             var arr = ns.split(".");
             if (arr.length == 1 && arr[0] == "Z") {
@@ -127,19 +127,19 @@
             }
             else {
                 var _fName = arr.splice(arr.length - 1, 1)[0];
-                var _ns = arr.join(".");  
-                var newFunc= Z.ns(_ns)[_fName]= (typeof func == 'function') ? func :function(){};
+                var _ns = arr.join(".");
+                var newFunc = Z.ns(_ns)[_fName] = (typeof func == 'function') ? func : function () { };
                 return newFunc;
             }
         }
     });
-	
-	/**
+
+    /**
 	 * Create Util Namespace.
 	 */
-	Z.ns("Util");
-	Z.apply(Z.Util, {
-		cookie: function (name, value, options) {
+    Z.ns("Util");
+    Z.apply(Z.Util, {
+        cookie: function (name, value, options) {
             if (typeof value != 'undefined') { // name and value given, set cookie
                 options = options || {};
                 if (value === null) {
@@ -192,6 +192,6 @@
                 return cookieValue;
             }
         }
-	});
+    });
 })();
 
